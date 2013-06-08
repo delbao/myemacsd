589ed56bd625d8f628c4c9ea6d3c8edafc9ca49f
dbao <dbao@ncsu.edu>
2013-06-07 21:23:32 -0700

    replace gtags.el with elpa version ggtags

--- programming.el
+++ programming.el
@@ -29,11 +29,8 @@
   (message "customizing GNU Emacs for OSX")
     ; anything special about Windows begins here 
     (add-hook 'c-mode-common-hook          ;; unlike setq, don't forget quote (') before c++-mode-hook
-        '(lambda() (ggtags-mode 1)   ;; get into gtags-mode whenever you get into c++-mode
-            (setq ggtags-global-command "/opt/local/bin/global")))
-    ; and ends here
+        '(lambda() (ggtags-mode 1)))   ;; get into gtags-mode whenever you get into c++-mode
   )
-
  )
 
 (message system-configuration)
@@ -42,11 +39,6 @@
 
 
         ; and end here
-
-;; hotkeys
-(global-set-key "\M-." 'ggtags-find-tag) ;; M-. finds tag
-(global-set-key [(control meta .)] 'ggtags-find-rtag) ;; C-M-. find all references of tag
-;;(global-set-key [(control meta ,)] 'gtags-find-symbol) ;; C-M-, find all usages of symbol
 
 ;;(setq gtags-select-buffer-single nil
 ;;       gtags-suggested-key-mapping t)
