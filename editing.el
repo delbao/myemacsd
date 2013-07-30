63e98264f7806a18f6cdac8ec8bca4b036835bad
dbao <dbao@ncsu.edu>
2013-07-30 07:02:34 -0700

    [EMACS]whole line or region cut/copy

--- editing.el
+++ editing.el
@@ -5,3 +5,18 @@
 
 ; set global tab width to 4, use by other modes
 (setq-default tab-width 4)
+
+; whole line or region cut/copy, not work in CUA
+(defun my-kill-ring-save (beg end flash)
+  (interactive (if (use-region-p)
+                   (list (region-beginning) (region-end) nil)
+                 (list (line-beginning-position)
+                       (line-beginning-position 2) 'flash)))
+  (kill-ring-save beg end)
+  (when flash
+    (save-excursion
+      (if (equal (current-column) 0)
+          (goto-char end)
+        (goto-char beg))
+      (sit-for blink-matching-delay))))
+(global-set-key [remap kill-ring-save] 'my-kill-ring-save)
