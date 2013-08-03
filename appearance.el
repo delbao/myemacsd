90255d806be211b953956786e86b11cfd6955751
dbao <dbao@ncsu.edu>
2013-08-02 21:25:05 -0700

    [EMACS]setup startup window layout for Windows

--- appearance.el
+++ appearance.el
@@ -9,8 +9,14 @@
     )
  ((string-match "nt6" system-configuration)
     (message "customizing GNU Emacs for Win NT")
-    (modify-frame-parameters nil '((width . 315) (height . 38) (top . 0) (left . -1230)))
-    )
+    (modify-frame-parameters nil '((width . 315) (height . 38) (top . 0) (left . -1235)))
+    (add-hook 'window-setup-hook ; due to init order
+        '(lambda ()
+           (split-window-horizontally 64)
+           (split-window-horizontally 22)
+           )
+        )
+ )
  ((string-match "darwin" system-configuration)
     (message "customizing GNU Emacs for OSX")
     ;; nil as frame to change init frame (selected-frame) size, can also use (make-frame) to create new frame
